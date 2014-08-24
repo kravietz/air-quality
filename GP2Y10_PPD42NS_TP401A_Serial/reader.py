ab2ab03ff100667005d3fa542684bf0f85006ae1
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-08-24 16:03:07 +0100

    upgrade to Python3 (tested on Python 3.4.0)

--- GP2Y10_PPD42NS_TP401A_Serial/reader.py
+++ GP2Y10_PPD42NS_TP401A_Serial/reader.py
@@ -1,11 +1,11 @@
-﻿#!/usr/bin/env python
+﻿#!/usr/bin/env python3
 # encoding: utf-8
 
 # https://github.com/kravietz/air-quality/
 
-import ConfigParser
+import configparser
 
-config = ConfigParser.RawConfigParser()
+config = configparser.RawConfigParser()
 config.read('reader.ini')
 
 # config file should look like this:
@@ -29,7 +29,7 @@ print(feed)
 
 import serial
 
-s = serial.Serial(serial_device, 115200)
+s = serial.Serial(serial_device, 9600)
 print(s)
 
 import datetime
@@ -39,40 +39,38 @@ q = []
 
 import csv
 import time
-<<<<<<< HEAD
 import os
 
-fout = open('{0}.csv'.format(time.time()), 'wb')
+fout = open('{0}.csv'.format(time.time()), 'wt')
 cw = csv.writer(fout)
 header = ['Time', 'GP2Y10', 'PPD42NS', 'TP401A']
 try:
-    cw.writeheader(header)
+    cw.writerow(header)
 except AttributeError:
     cw.writerow(header)
 
 while(True):
-	line = s.readline().strip()
-	now = datetime.datetime.utcnow()
+        line = s.readline().strip().decode('ascii')
+        now = datetime.datetime.utcnow()
 
-	if(line.startswith('#')):
-		print(line)
-		continue
+        if(line.startswith('#')):
+            print(line)
+            continue
 
-	gp,tp,pp = line.split(',')
-	gp,tp,pp = map(float, (gp,tp,pp))
+        try:
+            gp,tp,pp = line.split(',')
+            gp,tp,pp = list(map(float, (gp,tp,pp)))
+        except ValueError:
+            print("Invalid line", line)
+            continue
 
-	gp_voltage = gp*analog_voltage;	
-	gp_tsp = gp_voltage*200
-	gp_tsp2 = (gp_voltage-0.5)*200
+        gp_voltage = gp*analog_voltage;	
+        gp_tsp = gp_voltage*200
+        gp_tsp2 = (gp_voltage-0.5)*200
 
-	pp_tsp = (1.1*pow(pp, 3) - 3.8*pow(pp, 2) + (520*pp) + 0.62)*0.033
+        pp_tsp = (1.1*pow(pp, 3) - 3.8*pow(pp, 2) + (520*pp) + 0.62)*0.033
 
-
-	print("{8} Raw: {0} *** GP2Y10={1} {2}V {3}µg/m3 {4}µg/m3 *** PPD42NS={5} {6}µg/m3 *** TP401A={7}".format(line, gp, gp_voltage, gp_tsp, gp_tsp2, 
-													pp, pp_tsp,
-													tp,
-                                                                                                        now
-													))
+        print(u"{8} Raw: {0} *** GP2Y10={1} {2}V {3}µg/m3 {4}µg/m3 *** PPD42NS={5} {6}µg/m3 *** TP401A={7}".format(line, gp, gp_voltage, gp_tsp, gp_tsp2, pp, pp_tsp, tp, now))
         # add current results to queue
         q.append((now, gp, pp, tp))
         cw.writerow([now, gp, pp, tp])
@@ -80,16 +78,16 @@ while(True):
         fout.flush()
         os.fsync(fout.fileno())
 
-	try:
+        try:
             for n,g,p,t in q:
-	        feed.datastreams = [
-	            xively.Datastream(id='GP2Y10_dust',  current_value=g, at=n),
-	            xively.Datastream(id='PPD42NS_dust', current_value=p, at=n),
-	            xively.Datastream(id='TP401A_gas',   current_value=t, at=n),
-	        ]
-	        feed.update()
+                feed.datastreams = [
+                    xively.Datastream(id='GP2Y10_dust',  current_value=g, at=n),
+                    xively.Datastream(id='PPD42NS_dust', current_value=p, at=n),
+                    xively.Datastream(id='TP401A_gas',   current_value=t, at=n),
+                ]
+                feed.update()
             q = []
-	except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
-            print e
+        except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
+            print(e)
 
 
